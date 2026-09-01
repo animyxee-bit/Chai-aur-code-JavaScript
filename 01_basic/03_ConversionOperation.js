9c72f9cd4292070a702a92ccb7ff4962c2fc86a3
animyxee-bit <animyxee@gmail.com>
2026-09-01 09:29:57 +0000

    ConversionOperation of datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,24 @@
+let score = String
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "18" => 18
+// "18abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "92"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// "" => false
+// "Nikhil" => true
+
+let someNumber = 18
+let StringNumber = String(someNumber)
+console.log(StringNumber);
