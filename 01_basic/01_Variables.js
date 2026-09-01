beedacd9f60530bef6a13f173e6ee43a4be8f0f9
animyxee-bit <animyxee@gmail.com>
2026-09-01 08:38:30 +0000

    01 Variables

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 9999
+let accountEmail = "Nikhil@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+accountEmail = "Nikku@gmail.com"
+
+// accountId = 8 not allowed
+accountPassword = "54321"
+accountCity = "Benguluru"
+// console.log(accountEmail);
+console.table({accountEmail, accountPassword, accountCity})
