2698787476f3f8ea565c6560360d638b439bd054
animyxee-bit <animyxee@gmail.com>
2026-09-01 11:34:04 +0000

    ConversionOperation of datatypes

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -21,4 +21,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 18
 let StringNumber = String(someNumber)
-console.log(StringNumber);
+// console.log(StringNumber);
+
+
+//************************************* Operations **********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = 1
+let str2 = 8
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 +5);
+// console.log((1 + 2) + 5);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// let x = 100;
+// x--++
+// console.log(x--)(x++);
