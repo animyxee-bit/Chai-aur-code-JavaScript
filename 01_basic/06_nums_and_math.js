aad9f46c40332d6ced86cbc685b15bb9373ebc75
animyxee-bit <animyxee@gmail.com>
2026-09-03 11:59:35 +0000

    Math__and__Nums

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 59.9998
+
+// console.log(otherNumber.toPrecision(6))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString(`en-IN`));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(10.6));
+console.log(Math.ceil(10.1));
+console.log(Math.floor(10.6));
+console.log(Math.sqrt(25));
+
+
