7204ac0bc127e6c7b7c7815426803357ec94345a
animyxee-bit <animyxee@gmail.com>
2026-09-03 10:13:19 +0000

    Strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,9 @@
+const name = "nikhil"
+const repoCount = 100
+
+// console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my count is ${repoCount} `);
+const gameName = new String('permandevil')
+
+console.log(`${name} is ${age} years old.`)
+Nikhil is 25 years old.
