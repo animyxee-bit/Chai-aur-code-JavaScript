53a6f9d55e0ba53a13b3c218b7766a974cafeaca
animyxee-bit <animyxee@gmail.com>
2026-09-07 11:33:11 +0000

    Arrays p1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,25 @@
+// array //
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[0])
+const myHeroes = ["DrStrange", "ironman", "thor",]
+console.log(myHeroes[2]);
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2[2]);
+
+// Arrays methods //                                            
+
+// myArr.push(6)
+// myArr.push(10)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr.includes(10))
+// console.log(myArr.indexOf(5))
+// console.log(myArr)
+const newArr = myArr.join()
+console.log(newArr);
+console.log(myArr)
+
