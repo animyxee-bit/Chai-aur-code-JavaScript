309a3627b7a896e5997cfb0da940803725104d00
animyxee-bit <animyxee@gmail.com>
2026-09-07 11:32:29 +0000

    Date_and_Time

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,19 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toUTCString())
+// console.log(typeof myDate);
+let myCreatedDate = new Date("04-09-2026")
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
+
