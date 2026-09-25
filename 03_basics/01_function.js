6d21030c4752446c1d900552b52ea0d51de4291e
animyxee-bit <animyxee@gmail.com>
2026-09-25 13:28:40 +0000

    scopes_2

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -12,21 +12,21 @@ console.log("L");
 // sayMyName()
 
 function addTwoNumbers(number1, number2){
-//    let result = number1 + number2
-//    return result
+   let result = number1 + number2
+   return result
 return number1 + number2
      
 }
 const result = addTwoNumbers(5, 6)
-// console.log("Result: ", result);
+console.log("Result: ", result);
 
-// function loginUserMessage(username){
-//     if(username === undefined){
-//         console.log("Please enter something");
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter something");
         
-//     }
-//     return `${username} justlogged in`
-// }
+    }
+    return `${username} justlogged in`
+}
 function loginUserMessage(username = null){
     if(!username){
         console.log("Please enter something");
@@ -34,12 +34,12 @@ function loginUserMessage(username = null){
     }
     return `${username} justlogged in`
 }
-// console.log(loginUserMessage("Nikhil"))
+console.log(loginUserMessage("Nikhil"))
 
 function calculateCartPrice(val1, val2, ...num1){
     return num1 
 }
-// console.log(calculateCartPrice(200, 2000, 500, 1111111, 333333, 335555, 555555));
+console.log(calculateCartPrice(200, 2000, 500, 1111111, 333333, 335555, 555555));
 
 const user = {
     username: "Nikhil", 
@@ -50,7 +50,7 @@ function handleobject(anyobject){
     console.log(`User name is ${anyobject.username} and Price is ${anyobject.price}`);
     
 }
-//handleobject(user)
+handleobject(user)
 handleobject({
     username: "nikku",
     price: 2999
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,62 @@
+// const { use } = require("react");
+
+// const { use } = require("react");
+
+var c = 300
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30 ;
+    // console.log(c);
+    
+
+}
+
+// const all = (a, b, c)
+// console.log(all);
+
+
+// console.log(a)
+// console.log(b)  
+// console.log(c)
+
+function one() {
+    const username = "Nikhil"
+
+    function two() {
+        const website = "Youtube"
+        // console.log(username)
+    }
+    // console.log(website);
+
+    two()
+
+    
+}
+// one()
+
+if (true) {
+    const username = "Nikhil"
+    if (username === "Nikhil") {
+        const website = " Youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
+    
+    
+}
+// console.log(username);
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++ Interesting ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+addone(5)
+function addone(num) {
+    return num + 1
+}
+
+const addtwo = function(num) {
+    return num + 2
+}
+addtwo(5)
